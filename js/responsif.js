c67b4b2cfd94a70d8a97f52292c314d0d10bc31a
Arilalex96 <111502863+Arilalex96@users.noreply.github.com>
2023-04-27 19:51:09 +0800

    Update responsif.js

--- js/responsif.js
+++ js/responsif.js
@@ -1,8 +1,28 @@
 //menu
-var tombolMenu =
+var tombolMenu = $(".tombol-menu");
+var menu = $(".menu-bar ul");
+
+function klikMenu(){
+   tombolMenu.click(function(){
+      menu.toggle();
+   });
+   menu.click(function(){
+      menu.toggle();
+   });
+}
 
 $(document).ready(function() {
    var width = $(window).width();
-   alert('lebar'+width);
-}
-)
+   if(width < 990) {
+      klikMenu();
+})
+
+$(window).resize(function(){
+   var width = $(window).width();
+   if(width > 989){
+      menu.css("display","block");
+   } else {
+      menu.css("display","none");
+   }
+   klikMenu();
+});   
