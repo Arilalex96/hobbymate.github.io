0e4fbc3620f90380b68b78e8f61d228615750f6c
Arilalex96 <111502863+Arilalex96@users.noreply.github.com>
2023-04-29 17:28:45 +0800

    Add files via upload

---
+++ js/responsif.js
@@ -0,0 +1,28 @@
+var tombolMenu = $(".tombol-menu");
+var menu = $("nav .menu-bar ul");
+
+function klikMenu(){
+   tombolMenu.click(function(){
+      menu.toggle();
+   });
+   menu.click(function(){
+      menu.toggle();
+   });
+}
+
+$(document).ready(function() {
+   var width = $(window).width();
+   if(width < 991) {
+      klikMenu();
+      }
+})
+
+$(window).resize(function(){
+   var width = $(window).width();
+   if(width > 989){
+      menu.css("display","block");
+   } else {
+      menu.css("display","none");
+   }
+   klikMenu();
+});
